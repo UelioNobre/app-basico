3753bf26ab8afea1c7342879ea273a9be1993ce2
uelionobre <uelionobre@hotmail.com>
2024-06-05 19:51:40 -0300

    Remove hook de autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
-import useAuth from './data/hooks/useAuth';
 import { AuthProvider } from './data/context/AuthContext';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useAuth();
 
   return (
     <Router>
       <AuthProvider>
-        <AppRoutes isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+        <AppRoutes />
       </AuthProvider>
     </Router>
   );
